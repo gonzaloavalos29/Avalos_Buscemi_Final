45ec2851d1389b353ab74fa33423df88637a2917
Matibus-net <matibuscemi123@gmail.com>
2025-06-18 14:47:07 -0300

    .
    m

--- CentroEventos/CentroEventos.Aplicacion/obj/Debug/net8.0/CentroEventos.Aplicacion.AssemblyInfo.cs
+++ CentroEventos/CentroEventos.Aplicacion/obj/Debug/net8.0/CentroEventos.Aplicacion.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CentroEventos.Aplicacion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3af2a5fb0d307fe4d3b3320de7d7a918c48499f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1c59ab5e5ef568d5f60a1a861b1075e64176f58")]
 [assembly: System.Reflection.AssemblyProductAttribute("CentroEventos.Aplicacion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CentroEventos.Aplicacion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
